1ea2e6a02fa2c3705dba40f1f2f6b6b465749368
Rajvardhan <rajvardhanranawat80@gmail.com>
2024-07-20 17:42:53 +0530

    corrected speling

--- src/server/api/axiosClient.ts
+++ src/server/api/axiosClient.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Create an instance of axios
 export const apiClient = axios.create({
-  baseURL: process.env.NODE_ENV == 'production' ? NEXT_PUBLIC_URL :'http://localhost:3001/api', 
+  baseURL: process.env.NODE_ENV == 'production' ? process.env.NEXT_PUBLIC_URL :'http://localhost:3001/api', 
   timeout: 10000, withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
